241af5611351f7d56d48d30740a507a228e1a26e
designbyfaizi <faizanullah1999@gmail.com>
2022-05-21 19:14:14 +0500

    Re-Fixed CORS Issue

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ require('dotenv').config({path: __dirname + '/.env'})
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+  );
+  next();
+});
 
 // Routes
 const communityRoutes = require("./routes/community");
